835ba81b3351d38db00c68e64f8982c0368103d7
Yvanna21 <yvanna.mfout@facsciences-uy1.cm>
2024-12-19 17:01:03 +0100

    first Spring Boot project

--- Spring_boot/hello/src/main/java/com/example/hello/HelloController.java
+++ Spring_boot/hello/src/main/java/com/example/hello/HelloController.java
@@ -9,4 +9,26 @@ public class HelloController {
     public String hello(){
         return "Hello World!";
     }
+
+    @GetMapping("/json") // This will be the base URL for JSON response
+    public Message getMessage(){
+        return new Message("Hello World! This is a JSON response.");
+    }
+
+    //Intern class to represent a message
+    public static class Message {
+        private String text;
+
+        //Constructor
+        public  Message(String text){
+            this.text=text;
+        }
+        // Getter and Setter
+        public String getText(){
+            return text;
+        }
+        public void setText(String text){
+            this.text=text;
+        }
+    }
 }
